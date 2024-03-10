447f69a244d07e80ecf21ca1394f4ded9d7bb3f9
Guillem Puche <3519924+guillempuche@users.noreply.github.com>
2024-03-10 10:27:14 +0100

    refactor: delete ElectricSQL and Drizzle related code

--- src/auth.ts
+++ src/auth.ts
@@ -18,21 +18,6 @@ export class FakeConnector implements PowerSyncBackendConnector {
 	async uploadData(database: AbstractPowerSyncDatabase): Promise<void> {}
 }
 
-// // Generate an insecure authentication JWT.
-// // See https://electric-sql.com/docs/usage/auth for more details.
-// export const authToken = () => {
-// 	const subKey = '__electric_sub'
-// 	let sub = window.sessionStorage.getItem(subKey)
-// 	if (!sub) {
-// 		// This is just a demo. In a real app, the user ID would
-// 		// usually come from somewhere else :)
-// 		sub = genUUID()
-// 		window.sessionStorage.setItem(subKey, sub)
-// 	}
-// 	const claims = { sub }
-// 	return insecureAuthToken(claims)
-// }
-
 // export class FakeConnector implements PowerSyncBackendConnector {
 // 	async init() {
 // 		console.log('Initializing fake Supabase connector')
--- src/test.ts
+++
@@ -1,9 +0,0 @@
-import { db } from './db'
-
-console.log('Fetching...')
-try {
-	const quotes = await db.selectFrom('quotes').selectAll().execute()
-	console.log(`Quotes: ${quotes}`)
-} catch (error) {
-	console.error('Error fetching quotes:', error)
-}
