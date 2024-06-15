20006a976f55746b9b572b0e4f5838559c8f5e0b
Guillem Puche <3519924+guillempuche@users.noreply.github.com>
2024-06-15 15:31:37 +0200

    improv(web): add an identifier and disable autocomplete to Content Item component

--- apps/web/src/components/content_item.effect.tsx
+++ apps/web/src/components/content_item.effect.tsx
@@ -61,14 +61,18 @@ export const ContentItemEffect: React.FC<ContentItemProps> = ({
 			className={`rounded-2xl p-5 space-y-2 bg-green-50 dark:bg-stone-900 hover:bg-green-100 dark:hover:bg-green-900 ${className}`}
 		>
 			<input
+				autoComplete='off'
 				type='text'
+				data-component='quote-text'
 				value={editedText}
 				onChange={handleQuoteTextChange}
 				className='w-full p-2 rounded border-2 placeholder-gray-500 dark:placeholder-gray-400 border-gray-300 bg-white text-gray-900 dark:border-gray-600 dark:bg-stone-800 dark:text-gray-100'
 				placeholder='Edit quote text'
 			/>
 			<input
+				autoComplete='off'
 				type='text'
+				data-component='author-name'
 				value={editedAuthorName}
 				onChange={handleAuthorNameChange}
 				className='w-full p-2 rounded border-2 placeholder-gray-500 dark:placeholder-gray-400 border-gray-300 bg-white text-gray-900 dark:border-gray-600 dark:bg-stone-800 dark:text-gray-100'
